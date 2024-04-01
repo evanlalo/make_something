8006d2ba59c6ecc15f06b6d73112ee0769e30a42
Evan <evanslalo@gmail.com>
2024-03-31 20:11:38 -0400

    adds interceptor

--- lib/services/http/http.dart
+++ lib/services/http/http.dart
@@ -1,96 +1,45 @@
-// import 'package:dio/dio.dart';
-// final dio = Dio(BaseOptions(baseUrl: "http://127.0.0.1:8000"));
-
-// import 'package:dio/dio.dart';
-
-// class DioInterceptor extends Interceptor {
-//   Dio dio = Dio(BaseOptions(baseUrl: "http://127.0.0.1:8000"));
-
-//   @override
-//   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-//     options.headers.addAll({
-//       "Content-Type": "application/json",
-//       "Authorization": "Bearer ${token}",
-//     });
-//     // get token from the storage
-//     if (token != null) {
-//       options.headers.addAll({
-//         "Authorization": "Bearer ${token}",
-//       });
-//     }
-//     return super.onRequest(options, handler);
-//   }
-
-// }
-
 import 'package:dio/dio.dart';
-import 'package:make_something/auth/auth_scope.dart';
+import 'package:make_something/utils/logging.dart';
+import '../../auth/auth_scope.dart';
+import 'package:make_something/utils/constants.dart';
 
-class DioInterceptor extends Interceptor {
-  Dio dio = Dio(BaseOptions(baseUrl: "http://127.0.0.1:8000", headers: {
-    // 'Authorization': tokenMethod(),
-    "Content-Type": "application/json",
-    "Accept": "*/*",
-  }));
+Dio dio = Dio(BaseOptions(baseUrl: "$API_URL/api"))
+..interceptors.addAll([AuthInterceptor()]);
 
-
-  Future<String?> get tokenMethod async => "Bearer ${await StreamAuth.getToken()}";
+class AuthInterceptor extends Interceptor {
+  AuthInterceptor();
 
   @override
   void onRequest(
-      RequestOptions options, RequestInterceptorHandler handler) async {
-    print("HERERERER");
-    // String? token = await StreamAuth.getToken();
-    options.headers.addAll({
-      // "Authorization": "Bearer $token",
-      "Content-Type": "application/json",
-      "Accept": "*/*",
-    });
+      RequestOptions options,
+      RequestInterceptorHandler handler,
+      ) async {
 
-    return super.onRequest(options, handler);
+    // // create a list of the endpoints where you don't need to pass a token.
+    final listOfPaths = <String>[
+      '/token'
+    ];
+
+    // Check if the requested endpoint match in the
+    if (listOfPaths.contains(options.path.toString())) {
+      // if the endpoint is matched then skip adding the token.
+      return handler.next(options);
+    }
+
+    // Load your token here and pass to the header
+    String? token = await StreamAuth.getToken();
+    options.headers.addAll({'Authorization':  "Bearer $token"});
+    return handler.next(options);
   }
 
-  // @override
-  // void onError(DioException err, ErrorInterceptorHandler handler) async {
-  //   if (err.response?.statusCode == 401) {
-  //     // await refreshToken();
-  //     try {
-  //       handler.resolve(await _retry(err.requestOptions));
-  //     } on DioException catch (e) {
-  //       handler.next(e);
-  //     }
-  //     return;
-  //   }
-  //   handler.next(err);
-  // }
+  // You can also perform some actions in the response or onError.
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    return handler.next(response);
+  }
 
-  // Future<Response<dynamic>> refreshToken() async {
-  //   var response = await dio.post("refresh token URL",
-  //       options: Options(
-  //           headers: {"Refresh-Token": "get refresh token from your storage"}));
-
-  //   if (response.statusCode == 200) {
-  //     // LoginResponse loginResponse = LoginResponse.fromJson(response.data);
-
-  //     //   await setAuthenticationValuesInMemoryManagement(
-  //     //       accessToken: loginResponse.accessToken,
-  //     //       refreshToken: loginResponse.refreshToken);
-  //   }
-  //   return response;
-  // }
-
-  // Future<Response<dynamic>> _retry(RequestOptions requestOptions) async {
-  //   final options = Options(
-  //     method: requestOptions.method,
-  //     headers: {
-  //       "Authorization": "Bearer ${StreamAuth.getToken()}",
-  //       // "access-token": "Bearer ${MemoryManagement.getAccessToken()}"
-  //     },
-  //   );
-
-  //   return dio.request<dynamic>(requestOptions.path,
-  //       data: requestOptions.data,
-  //       queryParameters: requestOptions.queryParameters,
-  //       options: options);
-  // }
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    return handler.next(err);
+  }
 }
