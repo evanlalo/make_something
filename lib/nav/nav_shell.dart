a6341359bef8ea96948a798dbb46ecc30d5dc1f1
Evan <evanslalo@gmail.com>
2024-04-02 10:09:52 -0400

    checks admin route

--- lib/nav/nav_shell.dart
+++ lib/nav/nav_shell.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:make_something/nav/admin_bottom_nav.dart';
 import 'package:make_something/nav/app_bar.dart';
 import 'package:make_something/nav/bottom_nav.dart';
+import 'package:make_something/nav/drawer.dart';
+import 'package:make_something/routes.dart';
+import 'package:make_something/utils/logging.dart';
 
 class ScaffoldWithNestedNavigation extends StatelessWidget {
   const ScaffoldWithNestedNavigation({
@@ -12,9 +16,19 @@ class ScaffoldWithNestedNavigation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final location = GoRouter.of(context).location();
+
+    final isCurrentRouteAdmin = location.contains("/admin");
+    
+    logger.d("CURRENT ROUTE: $location");
+    logger.d("IS ADMIN ROUTE: $isCurrentRouteAdmin");
     return Scaffold(
         appBar: NavShellAppBar(key: const Key("app-bar")),
+        drawer: AppDrawer(),
         body: navigationShell,
-        bottomNavigationBar: NavShellNavBar(navigationShell: navigationShell));
+        bottomNavigationBar: isCurrentRouteAdmin ?
+        AdminNavShellNavBar(navigationShell: navigationShell)
+        :
+        NavShellNavBar(navigationShell: navigationShell));
   }
 }
