84c957c6657700cd9d6327a32a778c685a06f64b
Evan <evanslalo@gmail.com>
2024-08-24 12:50:09 -0400

    env file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:make_something/auth/auth_scope.dart';
 import 'package:make_something/routes.dart';
 import 'package:make_something/theme.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env", isOptional: true);
   runApp(StreamAuthScope(child: const MyApp()));
 }
 
