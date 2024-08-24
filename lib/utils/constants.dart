5befef5c8d101de336f56e5c57d2dc7dbe2b45fd
Evan <evanslalo@gmail.com>
2024-08-24 12:50:24 -0400

    uses const

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,1 +1,5 @@
-const API_URL = "http://127.0.0.1:8000";
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+String API_URL = dotenv.get("API_URL", fallback: "https://api-yardgames.darkmode.live");
