691a8a77cec66aecf23a4f64dec7b826de906a39
Evan <evanslalo@gmail.com>
2024-08-26 14:47:28 -0400

    dead

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,6 +1,4 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:make_something/services/http/http.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,32 +7,19 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-Future<Object> getData() async {
-  final response = await dio.get('/games');
-
-  return json.decode(response.toString());
-}
-
 class _HomeState extends State<Home> {
   late Future<Object> data;
 
   @override
   void initState() {
     super.initState();
-    data = getData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
         color: Theme.of(context).colorScheme.background,
-        child: FutureBuilder<dynamic>(
-          future: getData(),
-          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-            return Container(
-              color: Theme.of(context).colorScheme.background,
-            );
-          },
-        ));
+        child: const Text("Home")
+        );
   }
 }
