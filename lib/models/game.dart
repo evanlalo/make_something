12d2e47cf64cd82105b1f516edb9fd77dbb6a940
Evan <evanslalo@gmail.com>
2024-09-01 13:14:18 -0400

    changes to rules

--- lib/models/game.dart
+++ lib/models/game.dart
@@ -3,13 +3,13 @@ import 'package:json_annotation/json_annotation.dart';
 @JsonSerializable()
 class Game {
   final int id;
-  final String name, description;
+  final String name, rules;
   final bool active;
 
   Game(
       {required this.id,
       required this.name,
-      required this.description,
+      required this.rules,
       required this.active});
 
   factory Game.fromJson(Map<String, dynamic> json) => _$GameFromJson(json);
@@ -22,13 +22,13 @@ class Game {
 Game _$GameFromJson(Map<String, dynamic> json) => Game(
       id: json['id'] as int,
       name: json['name'] as String,
-      description: json['description'] as String,
+      rules: json['rules'] as String,
       active: json['active'] as bool,
     );
 
 Map<String, dynamic> _$GameToJson(Game instance) => <String, dynamic>{
       'id': instance.id,
       'name': instance.name,
-      'description': instance.description,
+      'rules': instance.rules,
       'active': instance.active,
     };
